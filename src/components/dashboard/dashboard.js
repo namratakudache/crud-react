f1d92be380eb512de549a19295e4794e0f125ef2
namrata <namratakudache09@gmail.com>
2025-06-03 20:11:59 +0530

    performed crud operations

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
+ import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
   const [users, setUsers] = useState([]);
+  const navigate=useNavigate()
 
-  useEffect(() => {
-    const fetchUsers = async () => {
+  const handleUpdate=(id)=>{
+    navigate(`/user/${id}`)
+  }
+  const handleDelete=async(id)=>{
+  try {
+        const response = await fetch(`http://localhost:5000/api/user/${id}`,{
+          method:'DELETE',
+        });
+        console.log(response)
+        if (response.ok) {
+         fetchUsers()
+        }
+        else {
+      const errorData = await response.json();
+      console.error("Delete failed:", errorData.error);
+    }
+        
+      } catch (error) {
+        console.error("Error while deleting users:", error.message);
+      }
+
+
+  }
+  const fetchUsers = async () => {
       try {
         const response = await fetch('http://localhost:5000/api/user');
         if (!response.ok) {
@@ -17,6 +41,7 @@ const Dashboard = () => {
         console.error("Error fetching users:", error.message);
       }
     };
+  useEffect(() => {
     fetchUsers();
   }, []);
   console.log(users)
@@ -28,7 +53,7 @@ const Dashboard = () => {
         <thead>
           <tr>
             <th>#</th>
-            <th>First Name</th>
+            <th>Name</th>
             <th>Email</th>
             <th>Phone</th>
           </tr>
@@ -40,6 +65,8 @@ const Dashboard = () => {
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>{user.phone}</td>
+              <button varient="dark" onClick={()=>handleUpdate(user._id)}>Update</button>
+                <button varient="dark" onClick={()=>handleDelete(user._id)}>Delete</button>
             </tr>
           ))}
         </tbody>
--- src/components/update/updateUser.js
+++ src/components/update/updateUser.js
@@ -1,11 +1,89 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import {useNavigate} from 'react-router-dom'
+// import './updateUser.css'
 
-const updateUser = () => {
+const UpdateUser = () => {
+  const navigate = useNavigate();
+  const {id}=useParams()
+   const [formData,setFormData]=useState({name:'',email:'',phone:''})
+
+
+    useEffect(() => {
+       const fetchUser = async () => {
+         try {
+           const response = await fetch(`http://localhost:5000/api/user/${id}`);
+           if (!response.ok) {
+             throw new Error('Failed to fetch users');
+           }
+           const data = await response.json();
+           setFormData(data);
+         } catch (error) {
+           console.error("Error fetching users:", error.message);
+         }
+       };
+       fetchUser();
+     }, [id]);
+
+    const handleInput=async(e)=>{
+    const {name,value}=e.target
+    setFormData({...formData,[name]:value})
+   
+  }
+   const handleSubmit=async(e)=>{
+    console.log("Form Submitted")
+    e.preventDefault();
+ 
+    try{
+      const response=await fetch(`http://localhost:5000/api/user/${id}`,{
+      method:'PATCH',
+      headers:{
+        'Content-type':'application/json'
+       
+      },
+      body:JSON.stringify(formData)
+    })
+    const data=await response.json()
+    console.log(data)
+    navigate('/')
+    }
+    catch(err){
+      console.error('Error in post user',err.message)
+    }
+ 
+  }
+    const handleUpdateUser=()=>{
+
+      navigate('/dashboard')
+    }
   return (
     <>
       <h1>Update User</h1>
+      
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="name" placeholder="Enter name" name="name" value={formData.name} onChange={handleInput}/>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" name="email" value={formData.email} onChange={handleInput} />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+      
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Phone</Form.Label>
+              <Form.Control type="phone" placeholder="phone" name="phone"  value={formData.phone} onChange={handleInput}/>
+            </Form.Group>
+           
+            <Button variant="primary" type="submit" onClick={handleUpdateUser}>
+              Update User
+            </Button>
+          </Form>
     </>
   )
 }
 
-export default updateUser
+export default UpdateUser
